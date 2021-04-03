5de48dbebf1d5619936b4e7ff99cee11e533ec70
Aliiboy <aliibotg3@gmail.com>
2021-04-03 02:38:06 +0200

    menu in python test

--- src/apps/core/apps.py
+++ src/apps/core/apps.py
@@ -5,13 +5,23 @@ stockage variables globales
 """
 
 from django.apps import AppConfig
-
+from django.utils.translation import gettext_lazy as _
 
 class CoreConfig(AppConfig):
     """
     Stockage metadonnee.
 
     (voir doc django)
-    """
+    """ 
 
     name = 'core'
+    login_url = reverse_lazy('dashboard:login')
+
+
+CORE_NAVIGATION = [
+    {
+        'label': _('Dashboard'),
+        'icon': 'fas fa-list',
+        'url_name': 'customer:profile_update',
+    },
+]
