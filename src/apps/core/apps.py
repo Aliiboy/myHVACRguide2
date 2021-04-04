4167f960916159275ba4b0b59b6e6d55d87b635f
Aliiboy <aliibotg3@gmail.com>
2021-04-04 19:47:48 +0200

    annule nav by python

--- src/apps/core/apps.py
+++ src/apps/core/apps.py
@@ -4,24 +4,16 @@ core App config.
 stockage variables globales
 """
 
+# > Django
 from django.apps import AppConfig
-from django.utils.translation import gettext_lazy as _
+
 
 class CoreConfig(AppConfig):
     """
     Stockage metadonnee.
 
     (voir doc django)
-    """ 
+    """
 
     name = 'core'
-    login_url = reverse_lazy('dashboard:login')
 
-
-CORE_NAVIGATION = [
-    {
-        'label': _('Dashboard'),
-        'icon': 'fas fa-list',
-        'url_name': 'customer:profile_update',
-    },
-]
--- src/apps/core/templatetags/core_tags.py
+++
@@ -1,9 +0,0 @@
-from django import template
-from utils.menu import get_nodes
-
-register = template.Library()
-
-
-@register.simple_tag
-def dashboard_navigation(user):
-    return get_nodes(user)
